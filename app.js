a259118276356d6da1ac2be2f4e91140cabb8edc
Lucas Hugdahl <lucashugdahl@Lucass-MacBook-Pro.local>
2019-09-06 23:41:09 -0700

    cors

--- app.js
+++ app.js
@@ -20,7 +20,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 //MAKE SURE THIS IS RESTRICTED IN PRODUCTION
 app.use(function(req, res, next) {
-	// res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+	res.header("Access-Control-Allow-Credentials", "true");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();
 });
 
